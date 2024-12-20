f250670fa255f60c20ed91121060c4be07fe49c3
Alangithub12 <alanakhmedov@MacBook-Air-Alan.local>
2024-12-20 21:47:52 +0300

    Создан класс ArticleView для отображения информации о статьях.

---
+++ article_view.py
@@ -0,0 +1,9 @@
+class ArticleView:
+    @staticmethod
+    def display_articles(articles):
+        for article in articles:
+            print(article)
+
+    @staticmethod
+    def display_message(message):
+        print(message)
